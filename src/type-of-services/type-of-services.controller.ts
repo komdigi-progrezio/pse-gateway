277ec70f860823c955bdb52e04472955f8955195
Vierry fadhillah <vierryfadhillah9@gmail.com>
2023-11-13 23:33:00 +0700

    add jenis layanan

--- src/type-of-services/type-of-services.controller.ts
+++ src/type-of-services/type-of-services.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
 import { Client, ClientProxy, Transport } from '@nestjs/microservices';
 
 @Controller('api/type-of-services')
@@ -8,6 +8,18 @@ export class TypeOfServicesController {
 
   @Post()
   async create(@Body() body: any) {
-    return this.client.send('', body);
+    return this.client.send('createTypeOfService', body);
+  }
+
+  @Delete('/:id')
+  async destroy(@Param('id') id: number) {
+    return this.client.send('removeTypeOfService', id);
+  }
+
+  @Post('/:id')
+  async update(@Body() body: any, @Param('id') id: number) {
+    body.id = id;
+
+    return this.client.send('updateTypeOfService', body);
   }
 }
