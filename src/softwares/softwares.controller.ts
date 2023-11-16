e9c3ea59330ef3f2d14114a8efaacd3e996f63b6
Vierry fadhillah <vierryfadhillah9@gmail.com>
2023-11-16 17:27:33 +0700

    add Perangkat Lunak Pendukung

--- src/softwares/softwares.controller.ts
+++ src/softwares/softwares.controller.ts
@@ -10,11 +10,27 @@ export class SoftwaresController {
   async create(@Body() body: any) {
     return this.client.send('createSoftware', body);
   }
+
+  @Post('/tools')
+  async createNetwork(@Body() body: any) {
+    return this.client.send('createSoftwareTool', body);
+  }
+  @Delete('/tools/:id')
+  async destroyNetworks(@Param('id') id: number) {
+    return this.client.send('removeSoftwareTool', id);
+  }
+
   @Delete('/:id')
   async destroy(@Param('id') id: number) {
     return this.client.send('removeSoftware', id);
   }
 
+  @Post('/tools/:id')
+  async updateNetwork(@Body() body: any, @Param('id') id: number) {
+    body.id = id;
+
+    return this.client.send('updateSoftwareTool', body);
+  }
   @Post('/:id')
   async update(@Body() body: any, @Param('id') id: number) {
     body.id = id;
